36b0c829f8f78ab5d0bcba59e5c7eb3e2bee6c45
shariful10 <freelancershariful10@gmail.com>
2025-04-22 01:33:40 +0600

    Create order

--- dist/app.js
+++ dist/app.js
@@ -14,7 +14,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: "https://books-shop-client.vercel.app",
     credentials: true,
 }));
 // Application routes
